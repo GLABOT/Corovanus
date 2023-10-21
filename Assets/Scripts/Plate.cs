91b1c9669e16d0cb63582d368ef055eae16d6e90
unlimpain <watniger@gmail.com>
2023-10-21 10:27:07 +0300

    add bool fields for animations

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using ScriptableObjects.Ingredients;
@@ -8,9 +7,12 @@ using UnityEngine;
 public class Plate : MonoBehaviour
 {
     [SerializeField] private int _maxIngredients;
+    [SerializeField] private float _ingredientsOffset;
+    [SerializeField] private Transform _ingredientsCreationPoint;
     private List<IngredientInfo> _ingredients;
-    private List<RecipeItemInfo> _allRecipesData; 
-    
+    private List<RecipeItemInfo> _allRecipesData;
+    private List<GameObject> _ingredientPrefabs;
+
     private void Awake()
     {
         _ingredients = new List<IngredientInfo>();
@@ -21,6 +23,7 @@ public class Plate : MonoBehaviour
     {
         if (_ingredients.Count<_maxIngredients)
             _ingredients.Add(ingredient);
+        RearrangeIngredients();
     }
     
     public void CleanPlate()
@@ -37,5 +40,37 @@ public class Plate : MonoBehaviour
                 return _allRecipesData[i];
         }
         return null;
+    }
+
+    private void RearrangeIngredients()
+    {
+        if (_ingredients.Count < 2) return;
+        for (int j = 0; j < _ingredients.Count; j++) // bubblesort ^.^
+        {
+            for (int i = 0; i < _ingredients.Count; i++)
+            {
+                if (_ingredients[i].priority < _ingredients[i + 1].priority)
+                {
+                    (_ingredients[i], _ingredients[i + 1]) = (_ingredients[i + 1], _ingredients[i]);
+                }
+            }
+        }
+
+        RecreateIngredients();
+    }
+
+    private void RecreateIngredients()
+    {
+        for (int i = 0; i < _ingredients.Count-1; i++)
+        {
+            Destroy(_ingredientPrefabs[i]);
+        }
+        
+        for (int i = 0; i < _ingredients.Count; i++)
+        {
+            Vector3 offset = new Vector3(0f, _ingredientsOffset * i, 0f);
+            _ingredientPrefabs[i] = Instantiate(_ingredients[i].prefab, 
+                _ingredientsCreationPoint.position + offset, Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using KitchenUnits;
+using KitchenUnits.ConcreteUnits;
 using ScriptableObjects.Ingredients;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,20 +12,37 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private GameObject targetPickUp;
+    public bool isCooking; // поля для анимаций нужны, изменяются прям тут
+    public bool isSinking;
+    public bool isChopping;
+    public bool isWalking;
+    public bool isHolding;
+    public bool isHoldingPlate;
+    public bool isHoldingTray;
+    public bool isHoldingIngredient;
     private Vector3 _movement;
-    private bool _isGrabbed;
-    private bool _isCooking;
     private GameObject _objectInHand;
-    
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
-        if (_isCooking) return;
+        if (isCooking)
+        {
+            isWalking = false;
+            return;
+        }
+        
         MovePlayer();
     }
 
     private void Update()
     {
-        if (_isCooking) return;
+        if (isCooking) return;
+        UpdateBooleans();
         RotatePlayer();
     }
 
@@ -33,6 +51,12 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(UserInput() * (_speed * Time.deltaTime), Space.World);
     }
 
+    private void UpdateBooleans()
+    {
+        isHolding = _objectInHand != null;
+        isWalking = _rigidbody.velocity == Vector3.zero;
+    }
+    
     private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))
@@ -40,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
 
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_movement), 0.15f);
     }
+    
     private Vector3 UserInput()
     {
         var horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -49,33 +74,42 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator Cook(float time, GameObject cookedIngredient)
     {
-        _isCooking = true;
+        isHolding = false;
+        isCooking = true;
         yield return new WaitForSeconds(time);
-        _isCooking = false;
+        isCooking = false;
+        isSinking = false;
+        isChopping = false;
         _objectInHand = Instantiate(cookedIngredient, targetPickUp.transform);
     }
     
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Box") && _objectInHand == null && Input.GetKey(KeyCode.E))
+        if (collision.gameObject.CompareTag("Box") && !isHolding && Input.GetKey(KeyCode.E))
         {
             var boxClass = collision.gameObject.GetComponent<BoxClass>();
             _objectInHand = Instantiate(boxClass.pickUpObject, targetPickUp.transform);
+            isHoldingIngredient = true;
         }
         //adding objectInHand to plate
-        if (collision.gameObject.CompareTag("Podnos") && _objectInHand != null && Input.GetKey(KeyCode.E))
+        if (collision.gameObject.CompareTag("Podnos") && isHolding && Input.GetKey(KeyCode.E))
         {
             var plate = collision.gameObject.GetComponent<Plate>();
             plate.AddIngredient(_objectInHand.GetComponent<Ingredient>().ingredient);
             Destroy(_objectInHand);
+            isHoldingIngredient = false;
         }
         //cooking something on something
-        if (collision.gameObject.CompareTag("KitchenUnit") && _objectInHand != null && Input.GetKey(KeyCode.E) && !_isCooking)
+        if (collision.gameObject.CompareTag("KitchenUnit") && isHoldingIngredient && Input.GetKey(KeyCode.E))
         {
             var kitchenUnit = collision.gameObject.GetComponent<KitchenUnit>();
             kitchenUnit.Cook(_objectInHand.GetComponent<Ingredient>().ingredient);
             Destroy(_objectInHand);
             StartCoroutine(Cook(kitchenUnit.timeToCook, kitchenUnit.CookedIngredient));
+            if (kitchenUnit.GetType() == typeof(Sink))
+                isSinking = true;
+            if (kitchenUnit.GetType() == typeof(SlicingTable))
+                isChopping = true;
         }
     }
 }
