43838fe78a6f84dbf66092e4eb9d4e9130c3064e
Dimapsv <64824559+Dimapsv@users.noreply.github.com>
2023-10-21 04:34:17 +0300

    5
    add something

--- Assets/ScriptableObjects/Ingredients/IngredientInfo.cs
+++ Assets/ScriptableObjects/Ingredients/IngredientInfo.cs
@@ -12,11 +12,13 @@ namespace ScriptableObjects.Ingredients
         [SerializeField] private IngredientInfo _slicedIngredient;
         [SerializeField] private IngredientInfo _deepFriedIngredient;
         [SerializeField] private IngredientInfo _washedIngredient;
+        [SerializeField] private int _priority;
         public string ingredientName => _ingredientName;
         public GameObject prefab => _prefab;
         public IngredientInfo grilledIngredient => _grilledIngredient;
         public IngredientInfo slicedIngredient => _slicedIngredient;
         public IngredientInfo deepFriedIngredient => _deepFriedIngredient;
         public IngredientInfo washedIngredient => _washedIngredient;
+        public int priority => _priority;
     }
 }
